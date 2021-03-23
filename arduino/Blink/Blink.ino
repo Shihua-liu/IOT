8b2bbf66ba0406303fe789a48ee4e761ba1b772f
shihua <liushihua2004@gmail.com>
2021-03-23 14:31:30 +0100

    les 5 af

---
+++ arduino/Blink/Blink.ino
@@ -0,0 +1,35 @@
+/*
+  ESP8266 Blink by Simon Peter
+  Blink the blue LED on the ESP-01 module
+  This example code is in the public domain
+
+  The blue LED on the ESP-01 module is connected to GPIO1
+  (which is also the TXD pin; so we cannot use Serial.print() at the same time)
+
+  Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
+*/
+uint8_t Led1 = D3;
+uint8_t Led2 = D5;
+uint8_t Led3 = D6;
+
+void setup() {
+  pinMode(Led1, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(Led2, OUTPUT);
+  pinMode(Led3, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(Led1, HIGH);
+  delay(2000);
+  digitalWrite(Led1, LOW);
+  delay(1000);
+  digitalWrite(Led2, HIGH);
+  delay(2000);
+  digitalWrite(Led2, LOW);
+  delay(1000);
+  digitalWrite(Led3, HIGH);
+  delay(2000);
+  digitalWrite(Led3, LOW);
+  delay(1000);
+}
